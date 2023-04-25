c210bab0ea4f7ad5579a42ad3d91795a1b7ff5c9
Cesar Julian <trabajos.cesar23@gmail.com>
2023-04-25 12:57:34 -0500

    gaa

--- database/connection.js
+++ database/connection.js
@@ -1,51 +1,9 @@
-// const mongoose = require("mongoose");
-// const url = "mongodb://localhost:27017/mi_red";
-// const connection = async () => {
-//   try {
-//     await mongoose.connect(url);
-//     console.log("Conectado corretamente mi bd : mi_red");
-//   } catch (error) {
-//     throw new Error("No se ha podido conectar a la base de datos !!");
-//   }
-// };
-
-// module.exports = connection;
-
 const mongoose = require("mongoose");
 const connection = async () => {
   await mongoose
-    .connect("mongodb://127.0.0.1:27017/mi_redsocial")
+    .connect(
+      "mongodb://mongo:ZbKtFklO6wvxOkeK13Ck@containers-us-west-98.railway.app:7654"
+    )
     .then(() => console.log("Connected!"));
 };
 module.exports = connection;
-
-// mongoose.connect(url, (err) => {
-//   if (err) {
-//     console.log("1");
-//   } else {
-//     console.log("2");
-//   }
-// });
-
-// const mongoose = require("mongoose");
-// const BD_URI = "mongodb://localhost:27017/mi_redsocial";
-// module.exports = () => {
-//   const connect = () => {
-//     mongoose.connect(
-//       BD_URI,
-//       {
-//         keepAlive: true,
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       },
-//       (err) => {
-//         if (err) {
-//           console.log("DB: ERROR");
-//         } else {
-//           console.log("Conexion correcta");
-//         }
-//       }
-//     );
-//   };
-//   connect();
-// };
